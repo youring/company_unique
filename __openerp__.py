072d79152aca7c429ed54da974c572e0d787d6af
youring <youring@gmail.com>
2013-09-29 11:45:48 +0800

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Unique Company Names',
+    'version': '1.0',
+    'author' : 'Youring',
+    'category': 'custom',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
